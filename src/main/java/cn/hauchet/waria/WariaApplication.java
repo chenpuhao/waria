25fa396fd4ceb8cced8f3a7b774d2b191b8d538b
亡灵 <chenpuhao1229@gmail.com>
2024-03-19 19:01:43 +0800

    Initial commit

---
+++ src/main/java/cn/hauchet/waria/WariaApplication.java
@@ -0,0 +1,13 @@
+package cn.hauchet.waria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/hauchet/waria/WariaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hauchet.waria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
